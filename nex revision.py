68e3b35e4b0bbc9040d28bfb84b7d085d49a07d4
sherimalik345678-jpg <sherimalik345678@gmail.com>
2026-04-01 11:33:36 +0500

    Add files via upload

---
+++ nex revision.py
@@ -0,0 +1,56 @@
+# num = 7
+# is_prime = True
+# for i in range(2, num):
+#     if num % i == 0:
+#         is_prime = False
+# print(is_prime)
+# _____________________________
+# a = [1,2,3,4]
+# b = [3,4,5]
+# common = []
+# for i in a:
+#     if i in b:
+#         common.append(i)
+# print(common)
+# # _____________________________
+# class Student:
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
+#     def display(self):
+#         print(self.name, self.marks)
+# s = Student("Ali", 90)
+# s.display()
+# # __________________________________
+# class A:
+#     def show(self):
+#         print("A")
+# class B:
+#     def display(self):
+#         print("B")
+# class C(A, B):
+#     pass
+# obj = C()
+# obj.show()
+# obj.display()
+# _______________________________
+# lst = [1,2,3,4,5,6]
+# result = [x**2 for x in lst if x % 2 == 0]
+# print(result)
+# ___________________________
+# t = (1,2,3,2,4)
+# print(t.count(2))
+# print(t.index(2))
+# ______________________
+# lst = [10, 20, 4, 45, 99]
+# lst = list(set(lst))
+# lst.sort()
+# print("Largest:", lst[-1])
+# print("Second Largest:", lst[-2])
+# _____________________
+# a = {1,2,3}
+# b = {2,3,4}
+# print(a | b)
+# print(a & b)  
+# print(a - b)  
+
